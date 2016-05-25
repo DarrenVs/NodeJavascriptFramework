99538000ec5f51c8993ddf665a3c0c1a01873271
Daniel <idaniel95@me.com>
2016-05-25 23:30:49 +0200

    edited camera, fixed chunk generation, added print

--- public/Worlds/BattleArena.js
+++ public/Worlds/BattleArena.js
@@ -9,126 +9,72 @@ Worlds[Enum.Worlds.BattleArena] = function( stage ) {
         1:Enum.ClassType.Unknown, //Wall
     }
     
-    var map = [
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1,1,0,1,1,0,1,1,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,1,0,0,1],
-        [1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-        [1,0,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,1,1,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-        [1,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1],
-        [1,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1],
-        [1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-        [1,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1],
-        [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1],
-        [1,0,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
-        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,1,0,0,0,1],
-        [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    ]
     mapSize = new Vector2.new(1250, 1250);
-    tileSize = new Vector2.new(
-        mapSize.x / map[0].length,
-        mapSize.y / map.length
-    )
-    
-    
-    for (var y = 0; y < map.length; y++) {
-        for (var x = 0; x < map[y].length; x++) {
-            
-            if (!Enum.ClassName[Tiles[map[y][x]]]) continue;
-            
-            var newObject = new Enum.ClassName[Tiles[map[y][x]]]({
-                size:tileSize,
-                position:Vector2.multiply(
-                    tileSize,
-                    new Vector2.new(x, y)
-                ),
-            })
-            
-            newObject.extends["collision"] = Collision(newObject);
-            newObject.anchored = true;
-            
-            stage.addChild( newObject );
-        }
-    }
     
     var player;
         
     var xLength = 10;
-    
-    spawnChunk(UncompressChunk("0101010101"));
+
+    spawnChunk(UncompressChunk("01010101010101010101"));
     
     //uncompress the chunk to a 2d array
     function UncompressChunk(stringToParse) {
         var yLength = stringToParse.length / xLength;
-
+        
         var counter = 0;
         
         var chunk = new Array();
         
         //assign every int in a 2 dimensional array
-        for (x = 0; x < xLength; x++) {
-            chunk[x] = new Array();
-            for (y = 0; y < yLength; y++) {
-                chunk[x][y] = stringToParse[counter];
+        for (y = 0; y < yLength; y++) {
+            chunk[y] = new Array();
+            for (x = 0; x < xLength; x++) {
+                console.log("y : " + y + " x : " + x + " input : " + stringToParse[counter]);
+                chunk[y][x] = stringToParse[counter];
                 counter++;
             }
         }
+        
         return chunk;
     }
-    
-    //var objectToSpawnNames = new Array();
-    //objectToSpawnNames[0] = new EmptyObject();
-    //objectToSpawnNames[1] = new Player();
-    var objectToSpawn = [new Player({
-                position: new Vector2.new(Math.random()*1000, Math.random()*1000),
-                size: new Vector2.new(15, 30),
-                colour: "red",
-            }), new Player({
-                position: new Vector2.new(Math.random()*1000, Math.random()*1000),
-                size: new Vector2.new(15, 30),
-                colour: "red",
-            }), new Player({
-                position: new Vector2.new(Math.random()*1000, Math.random()*1000),
-                size: new Vector2.new(15, 30),
-                colour: "red",
-            })];
     
     //excpects an int[,]
     function spawnChunk( chunkToSpawn ) {
 
         //calculate the y Length of this chunk, take its total length and divide it by its width.
-        var yLength = chunkToSpawn.length / xLength;
-
+        var yLength = (chunkToSpawn.length * chunkToSpawn[0].length) / xLength;
+        
+        tileSize = new Vector2.new(
+            mapSize.x / yLength,
+            mapSize.y / xLength
+        )
+        
         for (y = 0; y < yLength; y++)
         {
             for (x = 0; x < xLength; x++)
             {
-                if(chunkToSpawn[x][y] != 0) {
-                    console.log(objectToSpawn[1]);
-                    var spawnedObject = new objectToSpawn[chunkToSpawn[x][y] - 1];
-                    spawnedObject.position.x;
-                    spawnedObject.position.y;
-                }
+                if (!Enum.ClassName[Tiles[chunkToSpawn[y][x]]]) continue;
+            
+                var newObject = new Enum.ClassName[Tiles[chunkToSpawn[y][x]]]({
+                    size:tileSize,
+                    position:Vector2.multiply(
+                        tileSize,
+                        new Vector2.new(x, y)
+                    )
+                })
+
+                newObject.extends["collision"] = Collision(newObject);
+                newObject.anchored = true;
+                
+                stage.addChild( newObject );
             }
         }
     }
     
     
     this.update["BattleArenaUpdate"] = function() {
+        
+        stage.position.y ++;
         
         if (player == undefined || player.health <= 0) {
             
@@ -140,9 +86,5 @@ Worlds[Enum.Worlds.BattleArena] = function( stage ) {
             
             stage.addChild( player );
         }
-    }
-    
-    this.update["Scroller"] = function(Obj, deltaTime) {
-        //Obj.stage.position.y += 10000 * deltaTime;
     }
 }
