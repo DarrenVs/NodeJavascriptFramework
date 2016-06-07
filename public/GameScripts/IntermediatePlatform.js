75f5c6ea330ac1c1e050bcc90efb5ec4b745d7a4
Daniel <idaniel95@me.com>
2016-06-07 11:34:55 +0200

    added intermediatplatform function

--- public/GameScripts/IntermediatePlatform.js
+++ public/GameScripts/IntermediatePlatform.js
@@ -5,8 +5,16 @@ function IntermediatePlatform(properties) {
     var self = this;
     GameObject(this, properties);
     
+    this.extends = {
+        collision:Collision(this),
+    };
+    
     self.ClassType = Enum.ClassType.IntermediatePlatform;
     self.colliderType = Enum.colliderType.box;
     
     self.hitbox = Vector2.new(self.size.x, self.size.y);
+    
+    this.collisionEnter["IntermediatePlatformStay"] = function(Obj, Dir) {
+        
+    }
 }
