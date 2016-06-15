11c8c9fefa7bb827421fa02a1836cea618c6e29d
DavidZwit <davidzwitser@gmail.com>
2016-06-15 18:29:36 +0200

    refactoring statemachine

--- public/GameScripts/StateMachine/PickupStates.js
+++ public/GameScripts/StateMachine/PickupStates.js
@@ -1,140 +1,143 @@
 var PickupStates = {
     
     defaultState: function() {
-        var base = this.__proto__ = new State();
+        CreateState(this);
+        var base = this;
     },
     
-    idle: function() {
-        var base = this.__proto__ = new State();
-        var base 
-        this.Enter =  function(parent) {
-            base.Enter(parent);
-        }
+    idle: function(_parent) {
         
-        this.Reason = function () {        
-            return false;
-        }
+        CreateState(this);
     },
     
-    invulnerabilityOnHold: function() {
-        var base = this.__proto__ = new State();
-        
-        this.Reason = function () { 
+    invulnerabilityOnHold: function(_parent) {
+        CreateState(this);
+        var self = this;
+        var parent = _parent;
+
+        self.Reason = function () { 
             
             if(!INPUT_CLICK["18"]) {
                 return true;
             }
             
-            base.returnState = StatesEnum.invulnerabilityActivated;            
+            self.returnState = StatesEnum.invulnerabilityActivated;            
             return false;
         }
     },
         
     
-    invulnerabilityActivated: function() {
-        var base = this.__proto__ = new State();
+    invulnerabilityActivated: function(_parent) {
+        CreateState(this);
+        var self = this;
+        var parent = _parent;
         
-        this.invulnerabilityAmin;
-        this.maxInvulnerabilityTime = 400;
-        this.invulnerabilityCounter = 0;
+        self.invulnerabilityAmin;
+        self.maxInvulnerabilityTime = 400;
+        self.invulnerabilityCounter = 0;
           
-        this.Enter =  function(parent) {
-            base.Enter(parent);
+        self.Enter =  function() {
             
-            this.invulnerabilityAmin = new Enum.ClassName[Enum.ClassType.Platform_120x200]({
+            self.invulnerabilityAmin = new Enum.ClassName[Enum.ClassType.Platform_120x200]({
                 size: new Vector2.new( 40,  40),
-                position: new Vector2.new(base.parent.position.x, base.parent.position.y),
+                position: new Vector2.new(self.parent.position.x, self.parent.position.y),
             })
             
-            this.invulnerabilityCounter = 0;
+            self.invulnerabilityCounter = 0;
             console.log("start invul");
             
-            base.parent.walkSpeed *= 1.2;
+            self.parent.walkSpeed *= 1.2;
         }
         
-        this.Reason = function () { 
-            this.invulnerabilityCounter++;
-            if(this.maxInvulnerabilityTime < this.invulnerabilityCounter)
+        self.Reason = function () { 
+            self.invulnerabilityCounter++;
+            if(self.maxInvulnerabilityTime < self.invulnerabilityCounter)
                 return false;
             else 
                 return true;
         }
         
-        this.Leave =  function() {
-            this.invulnerabilityAmin.destroy();
+        self.Leave =  function(_parent) {
+            self.invulnerabilityAmin.destroy();
             
-            base.parent.walkSpeed /= 1.2;
-            console.log(base.parent.walkSpeed);
+            self.parent.walkSpeed /= 1.2;
+            console.log(self.parent.walkSpeed);
             print("end invul");
-            return base.Leave();
+            return self.returnState;
         }
     },
     
-    mineOnHold: function() {
-        var base = this.__proto__ = new State();
+    mineOnHold: function(_parent) {
+        CreateState(this);
+        var self = this;
+        var parent = _parent;
         
-        this.mine;
+        self.mine;
         
-        this.Reason = function () { 
+        self.Reason = function () { 
             console.log("mine");
             if(!INPUT_CLICK["18"]) {
                 return true;
             }
             
             mine = new Enum.ClassName[Mine]({
-                size: new Vector2.new(base.parent.size.x, base.parent.size.y),
+                size: new Vector2.new(self.parent.size.x, self.parent.size.y),
                 position: new Vector2.new(0, 0),
             })
                 
-            base.parent.stage.addChild( mine );
+            self.parent.stage.addChild( mine );
             
-            base.returnState = undefined;            
+            self.returnState = undefined;            
             return false;
         }
     },
         
-    ballOnHold: function() {
-        var base = this.__proto__ = new State();
+    ballOnHold: function(_parent) {
+        CreateState(this);
+        var self = this;
+        var parent = _parent;
         
-        this.ball;
+        self.ball;
         
-        this.Reason = function () { 
+        self.Reason = function () { 
             console.log("ball");
             if(!INPUT_CLICK["18"]) {
                 return true;
             }
             
             ball = new Enum.ClassName[Ball]({
-                size: new Vector2.new(base.parent.size.x, base.parent.size.y),
+                size: new Vector2.new(self.parent.size.x, self.parent.size.y),
                 position: new Vector2.new(0, 0),
             })
                 
-            base.parent.stage.addChild( ball );
+            self.parent.stage.addChild( ball );
             
-            base.returnState = undefined;            
+            self.returnState = undefined;            
             return false;
         }
     },
     
-    throwAbleOnHold: function() {
-        var base = this.__proto__ = new State();
+    throwAbleOnHold: function(_parent) {
+        CreateState(this);
+        var self = this;
+        var parent = _parent;
         
-        this.throwAble;
+        self.throwAble;
         
-        this.Reason = function () { 
+        self.Reason = function () { 
             console.log("throw");
             if(!INPUT_CLICK["18"]) {
                 return true;
             }
             
             throwAble = new Enum.ClassName[ThrowAbleObject]({
-                size: new Vector2.new(base.parent.size.x, base.parent.size.y),
+                size: new Vector2.new(self.parent.size.x, self.parent.size.y),
                 position: new Vector2.new(0, 0),
             })
                 
-            base.parent.stage.addChild( throwAble );
+            self.parent.stage.addChild( throwAble );
             
-            base.returnState = undefined;            
+            self.returnState = undefined;            
             return false;
         }
     },
