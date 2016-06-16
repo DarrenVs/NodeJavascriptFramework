56002482b02790c52c6468c2d639fd05f5a45eae
DavidZwit <davidzwitser@gmail.com>
2016-06-16 11:41:15 +0200

    fixed the game

--- public/main.js
+++ public/main.js
@@ -202,7 +202,7 @@ function Stage(properties) {
 
     self.DrawLoop = [];
 
-    self.gridSize = Vector2.new(10, 10);
+    self.gridSize = Vector2.new(601, 40);
     self.CollisionGrid = {
         grid:{
 
